f0762b5f8545cb767939f27bd0648df20e095f48
mengfu188 <1242733702@qq.com>
2020-05-29 17:12:12 +0800

    调整权限获取位置

--- Android/app/src/main/java/com/alibaba/android/mnnkit/demo/MainActivity.java
+++ Android/app/src/main/java/com/alibaba/android/mnnkit/demo/MainActivity.java
@@ -1,11 +1,13 @@
 package com.alibaba.android.mnnkit.demo;
 
+import android.Manifest;
 import android.content.Intent;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
+import com.alibaba.android.mnnkit.demo.utils.PermissionUtils;
 import com.alibaba.android.mnnkit.monitor.MNNMonitor;
 import com.tsia.example.mnnkitdemo.R;
 
@@ -22,7 +24,17 @@ public class MainActivity extends AppCompatActivity {
         }
 
         MNNMonitor.setMonitorEnable(true);
+        // Android M动态申请权限，摄像头+存储访问
+        PermissionUtils.askPermission(this,new String[]{Manifest.permission.CAMERA, Manifest
+                .permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE},10, initViewRunnable);
     }
+
+    private Runnable initViewRunnable = new Runnable() {
+        @Override
+        public void run() {
+
+        }
+    };
 
     public void onFaceDetection(View v) {
         startActivity(new Intent(MainActivity.this, FaceDetectionActivity.class));
